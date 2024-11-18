207c7be7eb5315da5f229e0c5fd11320b70357dd
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-11-18 20:06:25 +0000

    trying to add static in settings as was not working on heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -131,3 +131,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# static root not working 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
